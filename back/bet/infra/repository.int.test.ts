ea8ac2e882aefd81fd9e8da8e7f0e0e3ecf31568
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-01-22 12:59:58 +0100

    List last politics market

--- back/bet/infra/repository.int.test.ts
+++ back/bet/infra/repository.int.test.ts
@@ -4,9 +4,9 @@ import { findLatestPoliticalBet } from './repository.ts'
 
 test('findLatestPoliticalBet', async () => {
   // When
-  const bets = await findLatestPoliticalBet(Limit(20))
+  const bets = await findLatestPoliticalBet(Limit(2))
 
   // Then
-  expect(bets).toHaveLength(20)
+  expect(bets).toHaveLength(2)
   expect(bets[0]).toContainAllKeys(['id', 'title', 'description', 'endAt', 'updatedAt', 'yes', 'no'])
 })
--- back/bet/query.ts
+++ back/bet/query.ts
@@ -5,6 +5,6 @@ import { findLatestPoliticalBet } from './infra/repository.ts'
 export namespace Bet {
   export const getLatestPoliticalClosingIn7Day = async () => {
     const bets = await findLatestPoliticalBet(Limit(10))
-    return bets.filter(({ endDate }) => dayjs(endDate).isBefore(dayjs().add(7, 'day')))
+    return bets.filter(({ endAt }) => dayjs(endAt).isBefore(dayjs().add(7, 'day')))
   }
 }
