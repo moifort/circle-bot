d82d5d8ca9eaaebfc81c1527866364f04b1b7421
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-02-18 10:57:54 +0100

    ci: fix renovate workflow (#20)

--- back/src/utils/setup.test.ts
+++ back/src/utils/setup.test.ts
@@ -1,10 +1,14 @@
-import { afterEach } from 'bun:test'
+import { afterEach, beforeAll } from 'bun:test'
+import { type RulesTestEnvironment, initializeTestEnvironment } from '@firebase/rules-unit-testing'
 
-afterEach(async () => {
-  if (Bun.env.npm_lifecycle_event === 'test:feat') {
-    await fetch(
-      `http://${process.env.FIRESTORE_EMULATOR_HOST}/emulator/v1/projects/${process.env.GCLOUD_PROJECT}/databases/(default)/documents`,
-      { method: 'DELETE' },
-    )
-  }
-})
+if (Bun.env.npm_lifecycle_event === 'test:feat') {
+  let testEnv: RulesTestEnvironment
+
+  beforeAll(async () => {
+    testEnv = await initializeTestEnvironment({})
+  })
+
+  afterEach(async () => {
+    await testEnv.clearFirestore()
+  })
+}
