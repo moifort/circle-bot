56132966f57d6627998e726662abc9784ef7a2bd
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-02-19 10:49:01 +0100

    build(front): init biome (#30)

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,40 +1,36 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {ThemeProvider} from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Circle",
-  description: "Circle a Polymarket bot 🤖",
+	title: "Circle",
+	description: "Circle a Polymarket bot 🤖",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-          <html lang="en" suppressHydrationWarning>
-          <head />
-          <body>
-          <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-          >
-              {children}
-          </ThemeProvider>
-          </body>
-          </html>
-  );
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				<title>Circle</title>
+			</head>
+			<body>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
