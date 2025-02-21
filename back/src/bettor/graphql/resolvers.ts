308d7b5b4c0f992cf4ceda2bca54fbde5d9b150a
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-02-21 09:53:12 +0100

    chore(back): auto import resolvers and typedefs (#39)

---
+++ back/src/bettor/graphql/resolvers.ts
@@ -0,0 +1,8 @@
+export default {
+  Query: {
+    books: () => [
+      { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+      { title: 'Moby', author: 'Herman Melville' },
+    ],
+  },
+}
---
+++ back/src/bettor/graphql/typedefs.ts
@@ -0,0 +1,10 @@
+export default `#graphql 
+type Book {
+    title: String
+    author: String
+}
+
+type Query {
+    books: [Book]
+}
+`
