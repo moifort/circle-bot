7a18bce9fa5e174a90725db2c9094130749b9910
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-02-27 09:25:56 +0400

    ...

--- back/src/market/infra/repository.int.test.ts
+++ back/src/market/infra/repository.int.test.ts
@@ -1,11 +1,15 @@
 import { describe, expect, it } from 'bun:test'
+import { promises as fs } from 'node:fs'
+
+import path from 'node:path'
+import { AsyncParser } from '@json2csv/node'
 import { Limit } from '../../utils/index.validator'
 import type { ClosedBet } from '../index.type'
 import { BetId, BetOutcome } from '../index.validator'
 import { GammaApiRepository } from './repository'
 
 describe('GammaApiRepository', () => {
-  it('findBy', async () => {
+  it.skip('findBy', async () => {
     // When
     const closedBet = await GammaApiRepository.findBy(BetId('will-twitter-announce-bankruptcy-in-2023'))
 
@@ -19,11 +23,28 @@ describe('GammaApiRepository', () => {
     } as ClosedBet)
   })
 
-  it('findLatestPoliticalOpenBet', async () => {
+  it.skip('findLatestPoliticalOpenBet', async () => {
     // When
     const [bet] = await GammaApiRepository.findLatestOpenBet(Limit(100))
 
     // Then
     expect(bet).toContainAllKeys(['id', 'title', 'description', 'endAt', 'updatedAt', 'yes', 'no', 'status'])
+  })
+
+  it('findHistory', async () => {
+    // When
+    const historic = await GammaApiRepository.findHistoric(Limit(1000))
+
+    // Then
+    console.log(historic.length)
+    // const input = Readable.from(historic)
+    // const output = createWriteStream(path.join(__dirname, 'historic.csv'), { encoding: 'utf8' })
+
+    const opts = {}
+    const transformOpts = {}
+    const asyncOpts = {}
+    const parser = new AsyncParser(opts, asyncOpts, transformOpts)
+    const csv = await parser.parse(historic).promise()
+    await fs.writeFile(path.join(__dirname, 'historic.csv'), csv, 'utf8')
   })
 })
