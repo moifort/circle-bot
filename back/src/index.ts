77489f95bd9c303a1432bcf807ac9f3962c13ca5
Thibaut Mottet <thibaut.mottet@gmail.com>
2025-02-28 18:34:51 +0400

    fix point percentage

--- back/src/index.ts
+++ back/src/index.ts
@@ -31,19 +31,25 @@ const bettorIdFavorite = BettorId('19da3461-3e8e-4a58-823b-deed30dae53e')
 const walletIdFavorite = WalletId('1b33c582-1ae2-41b6-a9b2-ad5db5c7dafe')
 export const bot = process.env.FUNCTIONS_EMULATOR
   ? onRequest(async (_, response) => {
-      await Bot.runWithFavoriteStrategy(bettorIdFavorite, walletIdFavorite)
-      response.status(200).send('OK')
+      const result = await Bot.runWithFavoriteStrategy(bettorIdFavorite, walletIdFavorite)
+      response.status(200).send(result.isError() ? result.error : 'OK')
     })
-  : onSchedule('every day 06:00', async () => Bot.runWithFavoriteStrategy(bettorIdFavorite, walletIdFavorite))
+  : onSchedule('every day 06:00', async () => {
+      const result = await Bot.runWithFavoriteStrategy(bettorIdFavorite, walletIdFavorite)
+      console.log(`[BOT] ${result}`)
+    })
 
 const bettorIdJump = BettorId('d3c7e2d5-d118-4d04-8e52-1f5e33c7de14')
 const walletIdJump = WalletId('8132c695-2bcd-4d5a-aa49-3c0722a44956')
 export const botJump = process.env.FUNCTIONS_EMULATOR
   ? onRequest(async (_, response) => {
-      await Bot.runWithJumpStrategy(bettorIdJump, walletIdJump)
-      response.status(200).send('OK')
+      const result = await Bot.runWithJumpStrategy(bettorIdJump, walletIdJump)
+      response.status(200).send(result.isError() ? result.error : 'OK')
     })
-  : onSchedule('every minute', async () => Bot.runWithJumpStrategy(bettorIdJump, walletIdJump))
+  : onSchedule('every minute', async () => {
+      const result = Bot.runWithJumpStrategy(bettorIdJump, walletIdJump)
+      console.log(`[BOT] ${result}`)
+    })
 
 export const summarize = onRequest(async (request, response) => {
   const bettorId = BettorId(request.query.bettorId?.toString() ?? bettorIdFavorite)
